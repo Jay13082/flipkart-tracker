4878c0b1ec874bb02c29527613f67c266c78ea77
Jay13082 <97234785+Jay13082@users.noreply.github.com>
2025-07-26 15:52:20 +0530

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,12 @@
+# categories.py
+
+CATEGORIES = {
+    "Mobiles": "https://www.flipkart.com/search?q=mobiles",
+    "Laptops": "https://www.flipkart.com/search?q=laptops",
+    "Shoes": "https://www.flipkart.com/search?q=shoes",
+    "Sarees": "https://www.flipkart.com/search?q=sarees",
+    "Headphones": "https://www.flipkart.com/search?q=headphones",
+    "ACs": "https://www.flipkart.com/search?q=air+conditioners",
+    "Books": "https://www.flipkart.com/search?q=books",
+    "Washing Machines": "https://www.flipkart.com/search?q=washing+machines"
+}
